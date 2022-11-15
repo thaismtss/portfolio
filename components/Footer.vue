48b4bba5567d5a13ceaaeed0cd20608b70df85b7
Thais Martins <thaismartins1999@gmail.com>
2022-11-14 23:05:28 -0300

    add component footer

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">Desenvolvido por Thais Martins</div>
+</template>
+
+<style lang="scss">
+.footer {
+  height: 2em;
+  background-color: $piggy-pink;
+  text-align: center;
+  font-family: 'Lato';
+  color: $old-lavender;
+  padding-top: 0.5em;
+}
+</style>
